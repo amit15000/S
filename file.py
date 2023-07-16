8625df1ebb43281edb4aa5b96309cf77239129a9
Amit <amitmaurya15000@gmail.com>
2023-07-16 22:12:13 +0530

    Use line.strip()

--- file.py
+++ file.py
@@ -1,9 +1,12 @@
 f=open("d:\\amit.txt","r")
-line=''
+line=' '
 size=0
+size_after_strip=0
 while line:
     line=f.readline()
     l=len(line)
     size=size+l
+    size_after_strip=size_after_strip+len(line.strip())
     print(line)
 print("Total size of file is :", size)
+print("Total size after removing leading and trailing spaces :", size_after_strip)
